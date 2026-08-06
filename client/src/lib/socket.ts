89ade73847d064caa467cfa17ad9f1eda90decb7
flatwhiteeee <yostevaalvin01@gmail.com>
2026-08-07 01:05:46 +0700

    Connect frontend to Railway backend

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,5 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
-  autoConnect: false,
-});
+export const socket = io("https://guess-no-jutsu-production.up.railway.app");
